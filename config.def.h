1d9c7764a4e219f5c2d01b906446d4160882a464
Jayanth Parthsarathy <jparthaa@gmail.com>
2023-09-17 11:16:31 +0530

    Add color-message patch

--- config.def.h
+++ config.def.h
@@ -1,12 +1,21 @@
 /* user and group to drop privileges to */
-static const char *user  = "nobody";
-static const char *group = "nogroup";
+static const char *user  = "jayanth";
+static const char *group = "wheel";
 
 static const char *colorname[NUMCOLS] = {
-	[INIT] =   "black",     /* after initialization */
-	[INPUT] =  "#005577",   /* during input */
-	[FAILED] = "#CC3333",   /* wrong password */
+	[INIT] =   "#1e1e2e",     /* after initialization */
+	[INPUT] =  "#89b4fa",   /* during input */
+	[FAILED] = "#f38ba8",   /* wrong password */
 };
 
 /* treat a cleared input like a wrong password (color) */
 static const int failonclear = 1;
+
+/* default message */
+static const char * message = "Suckless: Software that sucks less.";
+
+/* text color */
+static const char * text_color = "#ffffff";
+
+/* text size (must be a valid size) */
+static const char * font_name = "-b&h-lucidatypewriter-medium-r-normal-sans-19-190-75-75-m-110-iso8859-15";
